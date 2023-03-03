b42e853df295191fde1498232f18b6d74521b590
ringo <m2052074@kinjo-u.ac.jp>
2023-03-03 10:20:38 +0900

    first commit

---
+++ chatapp.py
@@ -0,0 +1,23 @@
+import cgi
+
+print("Content-Type: text/html; charset=uft-8")
+print("")
+
+form = cgi.FieldStorage()
+
+if (not 'v1' in form) or (not 'v2' in form):
+    print("""
+    <form>
+    <input type="text" name="v1"> +
+    <input type="text" name="v2">
+    <input type="submit" value="計算">
+    </form>
+""")
+else:
+    v1 = form.getvalue("v1","0")
+    v2 = form.getvalue("v2","0")
+    try:
+        ans = int(v1) + int(v2)
+    except:
+        ans = 0
+    print("<h1>" , ans, "</h1>")
